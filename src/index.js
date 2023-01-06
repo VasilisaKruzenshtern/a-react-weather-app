9eb9e630ba9262630ef2dd9ab39f54f3e0d2d846
Елена Пальчикова <Parentsclub56@gmail.com>
2023-01-06 19:43:34 +0100

    Add project files.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+        <div className="App">
+            <div className="container">
+                <h1>World Weather</h1>
+                <App />
+                <div className="footer">
+                    <small>
+                        <a href="https://github.com/VasilisaKruzenshtern/react-weather-app">GitHub Weather App{" "}</a>
+                        by Olena Palchykova
+                    </small>
+                    
+                </div>
+            </div>
+            
+        </div>
+    </StrictMode>
+);
